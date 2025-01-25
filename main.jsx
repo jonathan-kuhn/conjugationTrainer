9f663757b900e0345e603cde7a1a08bd5900ac49
jonathan-kuhn <jonathan.simon.kuhn@gmail.com>
2025-01-25 13:53:16 +0100

    created some barebones content for all yet existing files

--- main.jsx
+++ main.jsx
@@ -1,1 +1,15 @@
+// index.jsx
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
 
+function App() {
+    return (
+        <div className="app">
+            <h1>Welcome to React!</h1>
+            <p>Start building your app by editing <code>index.jsx</code>.</p>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
